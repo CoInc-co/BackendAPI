f32664f2312e5ebd6ee20c888b0d71e1ecfb0e21
Scoutzknifez <codygaming@4alexanders.com>
2019-12-15 00:30:12 -0800

    Initial commit

---
+++ src/com/flex/BackendServer/Main.java
@@ -0,0 +1,8 @@
+package com.flex.BackendServer;
+
+public class Main {
+    public static void main(String[] args) {
+        // Get a connection to DB
+        // Establish a waiting thread for responding to queries
+    }
+}
