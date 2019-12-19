4b07a5b455277adf268100981f9ee1fd1edd81f8
Scoutzknifez <codygaming@4alexanders.com>
2019-12-19 13:37:39 -0800

    Update main

--- src/main/java/com/flex/BackendAPI/Main.java
+++ src/main/java/com/flex/BackendAPI/Main.java
@@ -3,11 +3,15 @@ package com.flex.BackendAPI;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * The start point of the backend server
+ */
 @SpringBootApplication
 public class Main {
     public static void main(String[] args) {
         // Run a separate thread to connect a singleton to database.
 
+        // Initializes the backend server and opens the connection for API calls.
         SpringApplication.run(Main.class, args);
     }
 }
